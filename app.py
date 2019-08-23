864aeb93fd251e6ac33395221b9e820cfd552db7
Ubuntu <ubuntu@ip-172-31-83-198.ec2.internal>
2019-08-23 22:08:38 +0000

    Materializecss version updated,Navbar and side-navbar fixed

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
-from flask import Flask, render_template, redirect, request, url_for
+from flask import (flash, Flask, redirect, render_template, request, session,
+                   url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
